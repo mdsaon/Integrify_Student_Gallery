ee620d16da060ab2273fa78a9c9064a6587c8aef
Saidur Rahman <swesaon@gmail.com>
2018-09-07 14:57:47 +0600

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path= require('path');
- const students = require('./routes/api/students');
+const students = require('./routes/api/students');
 const app = express();
 
 //app.use(express.static(path.join(__dirname, 'uploads')));
@@ -20,6 +20,15 @@ mongoose.connect(db, {useNewUrlParser: true})
         .catch(err => console.log(err));
 //use Routes
  app.use('/api/students' , students);
+ //serve static assets if in production
+ if(process.env.NODE_ENV === 'production') {
+   //set static folder
+   app.use(express.static('client/build'));
+
+   app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+   });
+ }
 //Run the application to the server
-        const port = process.env.PORT || 5000;
-        app.listen(port, () =>console.log(`server is running on ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () =>console.log(`server is running on ${port}`));
